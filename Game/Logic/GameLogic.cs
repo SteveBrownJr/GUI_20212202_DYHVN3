49d5a34655292d8e2243660ffc19c79fcc21c26a
barnai <barnai@stud.uni-obuda.hu>
2022-04-06 21:47:41 +0200

    I created layers.

---
+++ Game/Logic/GameLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Logic
+{
+    internal class GameLogic : IGameModel, IGameControl
+    {
+        string level;
+        int x;
+        int y;
+        public GameLogic()
+        {
+            
+        }
+    }
+}
