34c7e42c32fddbd9c134679c9f4b30f177cb6d48
barnai <barnai@stud.uni-obuda.hu>
2022-04-08 22:25:42 +0200

    controls enum added

--- Game/Renderer/GameRenderer.cs
+++ Game/Renderer/GameRenderer.cs
@@ -9,17 +9,42 @@ using System.Windows.Media;
 
 namespace Game.Renderer
 {
+    enum controls {UP,DOWN,LEFT, RIGHT,ATTACK,MELEE}
     internal class GameRenderer : FrameworkElement
     {
         IGameModel model;
+        IGameControl control;
         Size size;
         public void SetupModel(IGameModel model)
         {
             this.model = model;
         }
 
-        public GameRenderer()
+        public void PlayerControl(controls c)
         {
+            switch (c)
+            {
+                case controls.UP:
+
+                    break;
+                case controls.DOWN:
+
+                    break;
+                case controls.LEFT:
+
+                    break;
+                case controls.RIGHT:
+
+                    break;
+                case controls.ATTACK:
+
+                    break;
+                case controls.MELEE:
+
+                    break;
+                default:
+                    break;
+            }
         }
         public void Resize(Size size)
         {
@@ -27,10 +52,14 @@ namespace Game.Renderer
         }
         protected override void OnRender(DrawingContext drawingContext)
         {
+            
             base.OnRender(drawingContext);
+            
             if (model!=null && size.Width>50 && size.Height>50)
             {
-
+            
+                drawingContext.DrawRectangle(Brushes.Black, new Pen(Brushes.Black,0),new Rect(0, 0, size.Width, size.Height));
+                
             }
         }
     }
