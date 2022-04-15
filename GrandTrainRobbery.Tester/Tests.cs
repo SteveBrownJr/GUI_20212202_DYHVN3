46a3ce3b4fdff131e85c756dd0dde0f8ab3b5bab
SteveBrown <barnapisti19671@gmail.com>
2022-04-15 23:46:22 +0200

    ...

---
+++ GrandTrainRobbery.Tester/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandTrainRobbery.Tester
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void GravityTest()
+        {
+            
+        }
+
+    }
+}
---
+++ Tests/Tester.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    class Tester
+    {
+        [Test]
+        public void TestOne()
+        {
+            Assert.That(true);
+        }
+    }
+}
