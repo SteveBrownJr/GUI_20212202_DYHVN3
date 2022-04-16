7f8ce7e65cd635cf586d3719cd2e3dca6b150941
AksoR3kt <janosiakos@stud.uni-obuda.hu>
2022-04-16 15:33:32 +0200

    Menu with buttons created

---
+++ Game/Logic/MenuViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Game.Logic
+{
+    class MenuViewModel
+    {
+        public ICommand Level1Command { get; set; }
+        public ICommand Level2Command { get; set; }
+        public ICommand Level3Command { get; set; }
+        public ICommand ExitCommand { get; set; }
+
+        public MenuViewModel()
+        {
+            Level1Command = new RelayCommand(() =>
+            {
+                var lvl1Window = new MainWindow();
+                lvl1Window.Show();
+            });
+
+            ExitCommand = new RelayCommand(() => Environment.Exit(0));
+        }
+    }
+}
